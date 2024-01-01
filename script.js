9511f7277c6f25dd0c52dfeeeefe3ec8442ffa4d
Venureddyy <59521333+Venureddyy@users.noreply.github.com>
2024-01-01 23:39:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>S
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitHub Pages Test</title>
+</head>
+<body>
+    <h1>Hello, GitHub Pages!</h1>
+    <p>This is a simple webpage hosted on GitHub Pages.</p>
+    <script src="script.js"></script>
+</body>
+</html>
